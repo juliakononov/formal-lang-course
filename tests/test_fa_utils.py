d39b45b0b4ed75c2b65a9d4d69ded843caebe684
juliakononov <julickk04@mail.ru>
2024-09-14 19:10:13 +0300

    test: add more tests for fa_utils

--- tests/test_fa_utils.py
+++ tests/test_fa_utils.py
@@ -1,8 +1,10 @@
+import pytest
 import networkx as nx
 
 from project.fa_utils import regex_to_dfa
 from project.fa_utils import graph_to_nfa
 from project.graph_utils import read_graph_from_dot
+from project.graph_utils import get_graph
 from project.graph_utils import create_and_save_two_cycles_graph
 from pyformlang.finite_automaton import State
 from pyformlang.finite_automaton import Symbol
@@ -51,3 +53,21 @@ class TestGraphToNfa:
         assert len(nfa.start_states) == len(start_st)
         assert len(nfa.final_states) == len(final_st)
         assert len(set(int(st.value) for st in nfa.states)) == n1 + n2 + 1
+
+    @pytest.mark.parametrize(
+        "graph_name,start_st,final_st",
+        [
+            pytest.param("wc", {1}, {2}, id="wc_with__start_and_final"),
+            pytest.param("wc", set(), set(), id="wc"),
+        ],
+    )
+    def test_graph_to_nfa_from_dataset(self, graph_name: str, start_st: set[int], final_st: set[int]):
+        graph = get_graph(graph_name)
+        nfa = graph_to_nfa(graph, start_st, final_st)
+
+        if len(start_st) == len(final_st) == 0:
+            assert nfa.start_states == nfa.final_states == nfa.states
+        else:
+            assert len(nfa.start_states) == len(start_st)
+            assert len(nfa.final_states) == len(final_st)
+            assert len(set(int(st.value) for st in nfa.states)) == len(graph.nodes)
